a3687c2121190ca95da3bd8f2e3bbdb9cdfbe869
engineer1943@github.com <engineer1943@163.com>
2017-05-19 00:06:54 +0800

    refactoring test

--- lists/tests.py
+++ lists/tests.py
@@ -16,20 +16,6 @@ class HomePageTest(TestCase):
 
         self.assertEqual(Item.objects.count(),0)
 
-    def test_can_save_a_POST_request(self):
-        response=self.client.post('/',data={'item_text':'A new list item'})
-
-        self.assertEqual(Item.objects.count(),1)
-        new_item=Item.objects.first()
-        self.assertEqual(new_item.text,'A new list item')
-
-    def test_redirects_after_POST(self):
-        response=self.client.post('/',data={'item_text':'A new list item'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'],'/lists/the-only-list-in-the-world/')
-
-        #self.assertIn('A new list item', response.content.decode())
-        #self.assertTemplateUsed(response, 'home.html')
     def test_uses_list_template(self):
         response=self.client.get('/lists/the-only-list-in-the-world/')
         self.assertTemplateUsed(response, 'list.html')
@@ -61,3 +47,15 @@ class ItemModelTest(TestCase):
 
         self.assertEqual(first_saved_item.text,'The first (ever) list item')
         self.assertEqual(second_saved_item.text,'Item the second')
+class NewlistTest(TestCase):
+
+    def test_can_save_a_POST_request(self):
+        response=self.client.post('/lists/new',data={'item_text':'A new list item'})
+
+        self.assertEqual(Item.objects.count(),1)
+        new_item=Item.objects.first()
+        self.assertEqual(new_item.text,'A new list item')
+
+    def test_redirects_after_POST(self):
+        response=self.client.post('/lists/new',data={'item_text':'A new list item'})
+        self.assertRedirects(response,'/lists/the-only-list-in-the-world/')
